bc7dd752ec15b70a71f7123025abca46d5e684a8
haichenl <haichenl@andrew.cmu.edu>
2015-05-02 19:48:13 -0400

    code optimization

--- @RHF/SCF.m
+++ @RHF/SCF.m
@@ -7,22 +7,29 @@ oeiVec = reshape(obj.coreHamilt, [], 1);
 inv_S_Half = eye(size(obj.overlapMat)) / sqrtm(obj.overlapMat);
 
 occOrb = iniOrbital(:, 1:obj.numElectrons/2);
-densMat = occOrb * occOrb';
-densVec = reshape(densMat, [], 1);
+densVec = reshape(occOrb * occOrb', [], 1);
 elecEnergy = 0;
-
-fockSimVec = oeiVec + reshape(obj.DensToG(densMat), [], 1);
 
 % diis adiis
 cdiis = CDIIS(obj.overlapMat);
-adiis = ADIIS(fockSimVec);
+adiis = ADIIS(oeiVec);
 
-% obj.jkFactory.JK_Initialize('PKJK');
 for iter = 1:obj.maxSCFIter
+    fockVec = oeiVec + reshape(obj.DensToG(reshape(densVec, nbf, [])), [], 1);
+        
+    % diis extrapolate Fock matrix
+    cdiis.Push(fockVec, densVec); % density must be idempotent
+    if(cdiis.IAmBetter())
+        fockVec = cdiis.Extrapolate();
+    else
+        adiis.Push(fockVec, densVec); % Fock must be built from idempotent density
+        fockVec = adiis.Interpolate();
+    end
+    
     oldDensVec = densVec;
     oldElecEnergy = elecEnergy;
     [densVec, elecEnergy, orbital] ...
-        = DiagonalizeFock(reshape(fockSimVec, nbf, []), ...
+        = DiagonalizeFock(reshape(fockVec, nbf, []), ...
         inv_S_Half, obj.numElectrons);
     elecEnergy = oeiVec'*densVec + elecEnergy;
     
@@ -30,16 +37,6 @@ for iter = 1:obj.maxSCFIter
             && max(abs(densVec - oldDensVec)) < obj.MaxDensityThreshold ...
             && abs(elecEnergy - oldElecEnergy) < obj.EnergyThreshold)
         break;
-    end
-    fockVec = oeiVec + reshape(obj.DensToG(reshape(densVec, nbf, [])), [], 1);
-        
-    % diis extrapolate Fock matrix
-    cdiis.Push(fockVec, densVec); % density must be idempotent
-    if(cdiis.IAmBetter())
-        fockSimVec = cdiis.Extrapolate();
-    else
-        adiis.Push(fockVec, densVec); % Fock must be built from idempotent density
-        fockSimVec = adiis.Interpolate();
     end
 end
 hfEnergy = elecEnergy + obj.nucRepEnergy;
--- RHFGeomOpt.m
+++ RHFGeomOpt.m
@@ -33,35 +33,44 @@ classdef RHFGeomOpt < handle
             [~, iterRHF] = obj.rhf.SCF();
             disp(iterRHF)
             currGeom = obj.matpsi2.Molecule_Geometry();
+            obj.matpsi2.SCF_SetSCFType('RHF');
             obj.matpsi2.SCF_RunSCF();
             currForceVec = reshape(obj.matpsi2.SCF_Gradient(), [], 1);
             currHess = eye(length(currForceVec));
             for iter = 1:obj.maxIter
-                maxForce = max(abs(currForceVec));
-                rmsForce = sqrt(currForceVec' * currForceVec ./ length(currForceVec));
+                % next geometry
+                displaceVec = obj.stepSize .* (-currHess\currForceVec);
+                nextGeom = currGeom + reshape(displaceVec, [], 3);
                 
-                nextGeom = currGeom - obj.stepSize .* reshape(currHess\currForceVec, [], 3);
-                
+                % SCF and gradient of next geometry
                 obj.matpsi2.Molecule_SetGeometry(nextGeom);
-                
                 prevOrbital = obj.rhf.orbital;
                 obj.rhf = RHF.MatPsi2Interface(obj.matpsi2);
                 [~, iterRHF] = obj.rhf.SCF(prevOrbital);
-                obj.matpsi2.SCF_SetGuessOrb(prevOrbital);
+                obj.matpsi2.SCF_SetGuessOrb(obj.rhf.orbital);
                 obj.matpsi2.SCF_RunSCF();
                 nextForceVec = reshape(obj.matpsi2.SCF_Gradient(), [], 1);
                 
-                
+                % update Hessian
                 deltaForceVec = nextForceVec - currForceVec;
                 deltaGeomVec = reshape(nextGeom - currGeom, [], 1);
-                temp = deltaGeomVec'*currHess;
-                nextHess = currHess ...
+                temp = currHess * deltaGeomVec;
+                currHess = currHess ...
                     + (deltaForceVec*deltaForceVec')./(deltaForceVec'*deltaGeomVec) ...
-                    - (temp'*temp)./(deltaGeomVec'*currHess*deltaGeomVec);
+                    - (temp*temp')./(deltaGeomVec'*currHess*deltaGeomVec);
                 
-                displaceVec = reshape(nextGeom - currGeom, [], 1);
+                % convergence criteria
+                maxForce = max(abs(nextForceVec));
+                rmsForce = sqrt(mean(nextForceVec.^2));
                 maxDisplace = max(abs(displaceVec));
-                rmsDisplace = sqrt(displaceVec' * displaceVec ./ length(displaceVec));
+                rmsDisplace = sqrt(mean(displaceVec.^2));
+                
+                disp(iterRHF)
+                disp(maxForce)
+                disp(rmsForce)
+                disp(maxDisplace)
+                disp(rmsDisplace)
+                
                 if(maxForce < obj.thresMaxForce ...
                         && rmsForce < obj.thresRMSForce ...
                         && maxDisplace < obj.thresMaxDisplace ...
@@ -69,15 +78,10 @@ classdef RHFGeomOpt < handle
                     obj.finalMol = Molecule([obj.initialMol.cartesian(:,1), nextGeom.*obj.initialMol.Bohr2Angstrom]);
                     break;
                 end
+                
+                % start from next geometry
                 currGeom = nextGeom;
                 currForceVec = nextForceVec;
-                currHess = nextHess;
-                
-                disp(iterRHF)
-                disp(maxForce)
-                disp(rmsForce)
-                disp(maxDisplace)
-                disp(rmsDisplace)
             end
         end
         
